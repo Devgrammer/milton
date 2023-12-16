8524a03e5685b3f9ef6aa132edb4e9c13ec36ad1
Abhinav Rastogi <rastogi.abhinav8@gmail.com>
2023-12-16 17:06:07 +0530

    Data is updated

---
+++ src/data/featureData.jsx
@@ -0,0 +1,37 @@
+export const featureCardData =[{
+    img:0, 
+    headingColor:'peach', 
+    miniHeading:'Seamless Schedulling', 
+    subHeading:'Focus on what matters most for you', 
+    desc: 'Effortlessly plan your day with our intuitive drag-and-drop interface. Sync with multiple calendar platforms, import events from emails, and add participants with just a few clicks.', popup: true, popupIcon:'', 
+    popupContent:'I tested many calendar apps. This app is the best of all! It saves me hours of time.',
+    popupPersonDetail:'Carla, Head of Finance '
+},
+    {
+        img: 1,
+        headingColor: 'bluex',
+        miniHeading: 'Smart Reminders & Task',
+        subHeading: 'Never miss an important deadline or event again',
+        desc: "Never miss an important deadline again with our AI-driven  notifications. Our app intelligently analyzes your schedule to prioritize what's most important, keeping you on track and ensuring your day is productive and stress-free", popup: true, popupIcon: '',
+        popupContent: "I love how user-friendly this app is! It's so easy to add events and set reminders!",
+        popupPersonDetail: 'Adam, entrepreneur'
+    },
+    {
+        img: 2,
+        headingColor: 'voilet-lite',
+        miniHeading: 'Seamless Schedulling',
+        subHeading: 'Focus on what matters most for you',
+        desc: 'Effortlessly plan your day with our intuitive drag-and-drop interface. Sync with multiple calendar platforms, import events from emails, and add participants with just a few clicks.', popup: true, popupIcon: '',
+        popupContent: 'I tested many calendar apps. This app is the best of all! It saves me hours of time.',
+        popupPersonDetail: 'Carla, Head of Finance '
+    },
+    {
+        img: 3,
+        headingColor: 'peach',
+        miniHeading: 'Smart Reminders & Task',
+        subHeading: 'Never miss an important deadline or event again',
+        desc: "Never miss an important deadline again with our AI-driven  notifications. Our app intelligently analyzes your schedule to prioritize what's most important, keeping you on track and ensuring your day is productive and stress-free", popup: true, popupIcon: '',
+        popupContent: "I've tried a lot of calendar apps, but this one is by far the best! It's so intuitive and customizable, and it has all the features I need.",
+        popupPersonDetail: 'Annie, Designer'
+    },
+]
